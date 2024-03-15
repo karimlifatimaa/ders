3934fd626c7b37930761970f13f18d13be82ab73
asadabbasov2004 <>
2024-03-15 18:48:22 +0400

    axiri

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -4,20 +4,91 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(yoxlama("baki",'a'));
+            //Console.WriteLine(yoxlama("baki",'a'));
+            //Area(2);
+            //stringtrim(" fatime kerimli");
+            //String("Baki Dovlet Uni");
+        }
+        //public static bool yoxlama(string a ,char b)
+        //{
+        //    for(int i = 0; i < a.Length; i++)
+        //    {
+        //        if (a[i] == b)
+        //        {
+        //            return true;
+        //        }
 
-        }
-        public static bool yoxlama(string a ,char b)
-        {
-            for(int i = 0; i < a.Length; i++)
-            {
-                if (a[i] == b)
-                {
-                    return true;
-                }
-                
-            }
-            return false;
-        }
+        //    }
+        //    return false;
+        //}
+
+
+
+        //public static void Area(double r)
+        //{    
+        //    int p = 3;
+        //    double s = r * r * p;
+        //    Console.WriteLine(s);
+        //}
+        //public static void Area(double a, double b)
+        //{
+        //    double s=a * b;
+        //    Console.WriteLine(s);
+        //}
+        //public static void Area(double a, double b,double c)
+        //{
+        //    double s= 2 * ((a * b) + (a * c) + (b * c));
+        //    Console.WriteLine(s);
+        //}
+        //public static void Area(double a,double r, double b, double c)
+        //{
+        //int p = 3;
+        //    double sahe=p*r*r;
+        //    double yari = (a+b+c)/2;
+        //    double s = yari*sahe;
+        //    Console.WriteLine(s);
+        //}
+
+
+
+        //public static void StringTrim(string a)
+        //{
+        //    string b = "";
+        //    for (int i = 0; i < a.Length; i++)
+        //    {
+
+        //        if (a[i] == ' ')
+        //        {
+        //            continue;
+        //        }
+        //        b += a[i];
+        //    }
+        //    Console.WriteLine(b);
+        //}
+
+
+
+        //public static void String(string a)
+        //{
+        //    int temp = 0;
+        //    for(int i = 0; i < a.Length; i++)
+        //    {
+        //        if (a[i] != ' ')
+        //        {
+        //            Console.WriteLine(a[i]);
+        //            temp = i;
+        //            break;
+        //        }
+        //    }
+        //    for (int i = temp; i < a.Length-1; i++)
+        //    {
+        //        if (a[i]==' ' && a[i+1]!=' ')
+        //        {
+        //            Console.WriteLine(a[i+1]);
+        //        }
+        //    }
+        //}
+
+
     }
 }
